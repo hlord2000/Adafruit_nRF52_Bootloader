b57d2fdd1b0a8b300a0c42cc00903708a8f70cfd
Kelly Helmut Lord <helmut@helmutlord.com>
2023-12-18 01:39:08 -0500

    Added angry miao hatsu support

---
+++ src/boards/angry_miao_hatsu/board.h
@@ -0,0 +1,62 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Nick Winans
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _ANGRY_MIAO_HATSU_H
+#define _ANGRY_MIAO_HATSU_H
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+#define UICR_REGOUT0_VALUE UICR_REGOUT0_VOUT_3V3
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER       0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER    2 
+#define BUTTON_1          _PINNUM(1, 7)  // unusable: RESET
+#define BUTTON_2          _PINNUM(1, 8)  // no connection
+#define BUTTON_PULL       NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER  "Angry Miao"
+#define BLEDIS_MODEL         "Hatsu"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID           0x23A4
+#define USB_DESC_UF2_PID       0x00B7
+#define USB_DESC_CDC_ONLY_PID  0x00B7
+
+#define UF2_PRODUCT_NAME  "Angry Miao Hatsu"
+#define UF2_VOLUME_LABEL  "AM_HATSU"
+#define UF2_BOARD_ID      "nRF52840-AM-Hatsu"
+#define UF2_INDEX_URL     "https://www.angrymiao.com/am-hatsu/"
+
+#endif // _ANGRY_MIAO_HATSU_H
